dac6726c43a7c16689733989e92431ac650f7204
this-is-yello <yello_3617@naver.com>
2022-12-01 00:33:40 +0900

    221201 in Home

--- flutter_insta_prac_1/lib/screens/home_screen.dart
+++ flutter_insta_prac_1/lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'package:http/http.dart' as http;
 import 'dart:convert';
@@ -11,11 +12,11 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
   var data = [];
 
-  getData() async{
-    var result1 = await http.get(Uri.parse('https://codingapple1.github.io/app/data.json'));
+  getData() async {
+    var result1 = await http
+        .get(Uri.parse('https://codingapple1.github.io/app/data.json'));
     var result2 = jsonDecode(result1.body);
     setState(() {
       data = result2;
@@ -26,8 +27,9 @@ class _HomeScreenState extends State<HomeScreen> {
   var scroll = ScrollController();
   bool scrollEnd = true;
 
-  moreData() async{
-    var result3 = await http.get(Uri.parse('https://codingapple1.github.io/app/more1.json'));
+  moreData() async {
+    var result3 = await http
+        .get(Uri.parse('https://codingapple1.github.io/app/more1.json'));
     var result4 = jsonDecode(result3.body);
     print(result4);
     addData(result4);
@@ -45,75 +47,79 @@ class _HomeScreenState extends State<HomeScreen> {
     getData();
     scroll.addListener(() {
       print('scroll~');
-      if(scroll.position.pixels == scroll.position.maxScrollExtent && scrollEnd == true) {
+      if (scroll.position.pixels == scroll.position.maxScrollExtent &&
+          scrollEnd == true) {
         scrollEnd = false;
         print('bottom');
         moreData();
       }
     });
   }
-  
+
   @override
   Widget build(BuildContext context) {
-    if(data.isNotEmpty) {
+    if (data.isNotEmpty) {
       return Scaffold(
-        appBar: AppBar(
-          // leading: ,
-          title: Text('Instagram'),
-          actions: [
-            IconButton(
-              onPressed: (() {
-                
-              }),
-              icon: Icon(Icons.add_box_outlined)
-            )
-          ],
-        ),
-
-        body: ListView.builder(
-          controller: scroll,
-          itemCount: data.length,
-          itemBuilder: ((context, i) {
-            return Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  constraints: BoxConstraints(maxWidth: 500),
-                  child: Image.network(data[i]['image']),
-                ),
-                Container(
-                  constraints: BoxConstraints(maxWidth: 500),
-                  padding: EdgeInsets.all(8),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('좋아요' + ' ' + data[i]['likes'].toString() + '개'),
-                      Text(data[i]['user'], style: TextStyle(fontWeight: FontWeight.w800)),
-                      Text(data[i]['content'])
-                    ],
+          appBar: AppBar(
+            // leading: ,
+            title: Text('Instagram'),
+            actions: [
+              IconButton(onPressed: (() {}), icon: Icon(Icons.add_box_outlined))
+            ],
+          ),
+          body: ListView.builder(
+            controller: scroll,
+            itemCount: data.length,
+            itemBuilder: ((context, i) {
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    constraints: BoxConstraints(maxWidth: 500),
+                    child: Image.network(data[i]['image']),
                   ),
-                ),
-              ],
-            );
-          }),
-        ),
-
-        bottomNavigationBar: BottomNavigationBar(
-          items: [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home_outlined),
-              label: 'home',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.shopping_bag_outlined),
-              label: 'shop',
-            ),
-          ],
-        )
-      );
+                  Container(
+                    constraints: BoxConstraints(maxWidth: 500),
+                    padding: EdgeInsets.all(8),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('좋아요' + ' ' + data[i]['likes'].toString() + '개'),
+                        Padding(padding: EdgeInsets.only(bottom: 4)),
+                        MouseRegion(
+                          cursor: SystemMouseCursors.click,
+                          child: GestureDetector(
+                            child: Text(data[i]['user'],
+                                style: TextStyle(fontWeight: FontWeight.w800)),
+                            onTap: () {},
+                          ),
+                        ),
+                        Padding(padding: EdgeInsets.only(bottom: 4)),
+                        Text(data[i]['content'])
+                      ],
+                    ),
+                  ),
+                ],
+              );
+            }),
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+            items: [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home_outlined),
+                label: 'home',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.shopping_bag_outlined),
+                label: 'shop',
+              ),
+            ],
+          ));
     } else {
       return Center(
-        child: CircularProgressIndicator(color: Colors.black, ),
+        child: CircularProgressIndicator(
+          color: Colors.black,
+        ),
       );
     }
   }
